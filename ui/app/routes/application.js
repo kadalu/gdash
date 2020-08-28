6e7eb2085683648b41fe9e334ddaa008f84ef181
Aravinda Vishwanathapura <aravinda@kadalu.io>
2020-08-28 16:43:43 +0530

    Deleted Old UI code
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- ui/app/routes/application.js
+++
@@ -1,9 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Route.extend({
-    setupController: function(controller, model){
-        controller.load_time();
-        this._super(controller, model);
-    }
-});
-
