49b7143e0ec992c056f94f5ddd956ea236d41b03
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2022-12-12 22:37:24 -0500

    Add support for auto refresh every 60 seconds.
    Signed-off-by: Juan Calderon <jgcalderonperez@protonmail.com>

--- ui/src/components/last_updated.jsx
+++ ui/src/components/last_updated.jsx
@@ -5,23 +5,41 @@ import * as relativeTime from 'dayjs/plugin/relativeTime';
 export function LastUpdated({ setRefreshRequired }) {
     dayjs.extend(relativeTime);
 
+    const [autoRefresh, setAutoRefresh] = useState(false);
     const [lstUpdated, setLstUpdated] = useState(dayjs());
     const [lstUpdatedDisp, setLstUpdatedDisp] = useState(lstUpdated.fromNow());
 
     useEffect(() => {
-        setInterval(() => {
-            setLstUpdatedDisp(lstUpdated.fromNow());
-        }, 1000);
-    }, [lstUpdated]);
+        setLstUpdatedDisp(lstUpdated.fromNow());
+        let interval = null;
+
+        if (autoRefresh) {
+            interval = setInterval(() => {
+                setLstUpdated(dayjs());
+                setRefreshRequired(dayjs());
+            }, 60000);
+        } else {
+            clearInterval(interval);
+            interval = null;
+        }
+
+        return () => clearInterval(interval);
+    }, [autoRefresh, lstUpdated, setRefreshRequired]);
 
     function handleClick() {
         setLstUpdated(dayjs());
         setRefreshRequired(dayjs());
     }
 
+    function toggleAutoRefresh() {
+        setAutoRefresh(!autoRefresh);
+    }
+
     return (
         <p className="text-indigo-900 px-4 py-1 text-right text-sm mt-5">
-            Last updated {lstUpdatedDisp} <button className="bg-indigo-600 text-indigo-100 rounded-lg px-2 py-1 hover:bg-indigo-900" onClick={handleClick}>reload</button>
+            Last updated {lstUpdatedDisp}
+            <button className="bg-indigo-600 text-indigo-100 rounded-lg px-2 py-1 hover:bg-indigo-900 mx-2" onClick={handleClick}>reload</button>
+            <button className="bg-indigo-600 text-indigo-100 rounded-lg px-2 py-1 hover:bg-indigo-900" onClick={() => toggleAutoRefresh()}>auto refresh</button>
         </p>
     );
 }
