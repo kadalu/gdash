ecdab7c22cbc29d87b0df54985318482297e25aa
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2023-06-27 09:04:18 -0400

    Use secure headers

--- gdash/__main__.py
+++ gdash/__main__.py
@@ -25,6 +25,7 @@ conf = {
         "tools.sessions.on": True,
         "tools.sessions.secure" = True,
         "tools.sessions.httponly" = True,
+        "tools.secureheaders.on" = True,
         "tools.staticdir.dir": os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "ui"
         ),
